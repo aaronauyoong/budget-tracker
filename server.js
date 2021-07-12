e616d8dcedd906fd95f7909815029aa2ee14d1ad
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-12 12:39:10 +0800

    added dotenv for production build

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+require("dotenv").config();
+
 const PORT = 3000;
 
 const app = express();
@@ -15,14 +17,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });
